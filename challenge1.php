46dd0e8c07b7c559dba88cba0faa2d88c80ccc00
Mike Gerlinger <bouarroudjmike@yahoo.fr>
2024-10-16 10:09:48 +0200

    new challenge

---
+++ challenge1.php
@@ -0,0 +1,15 @@
+<?php
+
+$namesOfBooks= "Voyage avec un âne dans les Cévennes";
+echo $namesOfBooks. "<br>";
+
+$readBooks= "false";
+echo $readBooks. "<br>";
+
+$yearsOfThisBooks= 2017;
+echo $yearsOfThisBooks. "<br>";
+
+$priceBooks= "6.20€";
+echo $priceBooks . "<br>";
+
+?>
